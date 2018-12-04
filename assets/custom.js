e2d7437a22174a3310c47540087da8088612c8e8
Arslan Ismail <arslan.ismail@nxb.com.pk>
2018-12-04 20:57:06 +0500

    custom js with better structure

--- assets/custom.js
+++ assets/custom.js
@@ -2,30 +2,50 @@
 // =================  Toggel Fucntion For Vat Prices Effected files are header.liquid (dropdown) and
 // =================  Product-price.liquid (pricing and filters + class names (incvat,exvat))
 
-function vatPricing(){
-    console.log('in here');
-    var ele = document.getElementsByClassName('incvat');
-    // var showPrice=document.getElementById('incvat');
-    var hideprice=document.getElementsByClassName('excvat');  
-    var x = document.getElementById("vatid").value;
 
-    if(x=="inc"){
-  
-        for (var i = 0; i < ele.length; i++ ) {
-            ele[i].style.display = "block";
-            hideprice[i].style.display = "none";
-        }
-        
-    }
-    else if(x=="exc")
+var vatApp=(function(){
+       // Method To Control Pricing  
+    function vatPricing()
     {
-        for (var i = 0; i < hideprice.length; i++ ) {
-            hideprice[i].style.display = "block";
-            ele[i].style.display = "none";
+        
+        var ele = document.getElementsByClassName('incvat');
+        // var showPrice=document.getElementById('incvat');
+        var hideprice=document.getElementsByClassName('excvat');  
+        var x = document.getElementById("vatid").value;
+    
+        if(x=="inc"){
+      
+            for (var i = 0; i < ele.length; i++ ) {
+                ele[i].style.display = "block";
+                hideprice[i].style.display = "none";
+            }
+            
         }
+        else if(x=="exc")
+        {
+            for (var i = 0; i < hideprice.length; i++ ) {
+                hideprice[i].style.display = "block";
+                ele[i].style.display = "none";
+            }
+            
+        }
+        else{
+            console.log('Please choose right options');
+        }
+    }
+
+//   ==============   INITIALIAZE APP. ====================
+    function init()
+    {
         
     }
-    else{
-        console.log('choose right options');
+    return {
+        init:init,
+        pricing:vatPricing
     }
-}
+
+})();
+
+vatApp.init();
+
+
